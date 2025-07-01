dfee35eb0fc97adb7281336c429f4016020e5537
Queen Diana <dianadenzyy@gmail.com>
2025-07-01 17:30:10 -0400

    Actualizar crick.js

--- plugins/crick.js
+++ plugins/crick.js
@@ -28,7 +28,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
       const result = response.data;
 
       let formattedResult = `╭══════════════•∞•══╮\n`;
-      formattedResult += `│⿻   *TOXIC MD 😎 🔥*\n`;
+      formattedResult += `│⿻   *DIANA MD 😎 🔥*\n`;
       formattedResult += `│⿻   *LIVE MATCH INFO* ✨\n`;
       formattedResult += `│⿻\n`;
 
